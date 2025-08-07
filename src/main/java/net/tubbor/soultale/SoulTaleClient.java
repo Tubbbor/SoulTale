5d143f212f0aecf2f63b22a7bdac37953254828b
eunse <kygokay05@gmail.com>
2025-08-07 14:10:52 +0300

    07/08/2025

---
+++ src/main/java/net/tubbor/soultale/SoulTaleClient.java
@@ -0,0 +1,10 @@
+package net.tubbor.soultale;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulTaleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tubbor/soultale/SoulTaleDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tubbor.soultale;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SoulTaleDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
